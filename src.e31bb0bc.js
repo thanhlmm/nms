2cbd0824b3c13c89990c287e8b3add33af642ce7
DaoThang <thangdao1983@gmail.com>
2021-07-16 12:06:04 +0700

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -17093,7 +17093,7 @@ __exportStar(require("./browser-connect"), exports);
 require("error-polyfill");
 
 },{"./key_stores/browser-index":"../node_modules/near-api-js/lib/key_stores/browser-index.js","./common-index":"../node_modules/near-api-js/lib/common-index.js","./browser-connect":"../node_modules/near-api-js/lib/browser-connect.js","error-polyfill":"../node_modules/error-polyfill/index.js"}],"config.js":[function(require,module,exports) {
-var CONTRACT_NAME = "dev-1626400442960-15347105963967" || 'sms-tool';
+var CONTRACT_NAME = "dev-1626400442960-15347105963967" || 'dev-1626400442960-15347105963967';
 
 function getConfig(env) {
   switch (env) {
@@ -17527,7 +17527,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57574" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55665" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
