57d624bf5050b5cbbe1949dda3e5fefb720929c0
DaoThang <thangdao1983@gmail.com>
2021-07-29 18:15:35 +0700

    Update comments

--- contract/assembly/model.ts
+++ contract/assembly/model.ts
@@ -1,7 +1,7 @@
 import { env, PersistentVector, PersistentMap } from "near-sdk-as";
 
 /**
- * A data structure that stores the content of a message
+ * A data structure that stores the information of a message
  */
 @nearBindgen
 export class Message {
@@ -23,10 +23,10 @@ export class Message {
 // An array that stores messages on the blockchain
 export const messages = new PersistentVector<Message>("psms");
 
-// Store the mapping between the account and the array containing the sent message indexes.\
+// Store list of message indexes for each sender's account.\
 // Note: This avoids using a for loop to search for data, and also reduces duplication of message data
 export const sentInfos = new PersistentMap<string, PersistentVector<i32>>("psent");
 
-// Store the mapping between the account and the array containing the inbox message indexes.\
+// Store list of message indexes for each sender's account.\
 // Note: This avoids using a for loop to search for data, and also reduces duplication of message data
 export const inboxInfos = new PersistentMap<string, PersistentVector<i32>>("pinbox");
